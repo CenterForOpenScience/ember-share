5aec51eb8a32fc40f2157a7277bd7a0c26c9e52d
Abram Booth <abram@cos.io>
2016-06-28 15:53:34 -0400

    Add model base mixins

---
+++ tests/unit/mixins/share-object-mixin-test.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import ShareObjectMixinMixin from 'ember-share/mixins/share-object-mixin';
+import { module, test } from 'qunit';
+
+module('Unit | Mixin | share object mixin');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let ShareObjectMixinObject = Ember.Object.extend(ShareObjectMixinMixin);
+  let subject = ShareObjectMixinObject.create();
+  assert.ok(subject);
+});
