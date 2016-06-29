f2c1168d3995cc3183e0207ff8a47cd41f44040f
Henrique Moco <hmoco94@vt.edu>
2016-06-29 15:44:28 -0400

    mirage

---
+++ mirage/config.js
@@ -0,0 +1,31 @@
+export default function() {
+    this.get('/');
+    this.get('/normalized');
+    this.get('/normalized/:id');
+    this.get('/changeset');
+    this.get('/changeset/:id');
+    this.get('/change');
+    this.get('/change/:id');
+    this.get('/venue');
+    this.get('/venue/');
+    this.get('/institution');
+    this.get('/institution/:id');
+    this.get('/manuscript');
+    this.get('/manuscript/:id');
+    this.get('/preprint');
+    this.get('/preprint/:id');
+    this.get('/creative_work');
+    this.get('/creative_work/:id');
+    this.get('/tag');
+    this.get('/tag/:id');
+    this.get('/taxonomy');
+    this.get('/taxonomy/:id');
+    this.get('/data_provider');
+    this.get('/data_provider/:id');
+    this.get('/award');
+    this.get('/award/:id');
+    this.get('/funder');
+    this.get('/funder/:id');
+    this.get('/raw');
+    this.get('/raw/:id');
+}
