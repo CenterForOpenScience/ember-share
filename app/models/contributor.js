259ddec000bcc19377bcff0c6c75e8edd78ad014
Abram Booth <abram@cos.io>
2016-06-29 14:44:00 -0400

    Remove through models

--- app/models/contributor.js
+++ app/models/contributor.js
@@ -3,6 +3,6 @@ import ShareObjectMixin from '../mixins/share-object';
 
 export default DS.Model.extend(ShareObjectMixin, {
     person: DS.belongsTo('person'),
-    creativeWork: DS.belongsTo('abstractCreativeWork', { polymorphic: true }),
+    creativeWork: DS.belongsTo('abstract-creative-work', { polymorphic: true }),
     url: DS.attr('string')
 });
