ebc44b8e932922229bec05d815a7cd3b1b369b28
Henrique Moco <hmoco94@vt.edu>
2016-07-02 16:51:53 -0400

    pass query to buildUrl, where query url is built

--- app/components/search-result/component.js
+++ app/components/search-result/component.js
@@ -3,7 +3,7 @@ import layout from './template';
 
 export default Ember.Component.extend({
     layout,
-    abbreviate: Ember.computed('params.[]', function(){
+    abbreviation: Ember.computed('params.[]', function(){
         let desc = this.get('obj.description');
         return desc.length > 350 ? desc.substring(0, 350) + '...' : desc
     }),
