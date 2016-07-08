ff5b72285b77bfd7653cfd61ae621f9171c4a2ce
Abram Booth <abram@cos.io>
2016-07-08 09:47:34 -0400

    Search bar styling, work type

--- app/components/search-facet-daterange/component.js
+++ app/components/search-facet-daterange/component.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Component.extend({
+    init() {
+        this._super(...arguments);
+        this.set('selectedRange', this.get('dateRanges')[0]);
+    },
+
+    dateRanges: Ember.computed(function() {
+        return [
+            { name: 'All time' },
+            { name: 'Past week', range: { gte: 'now-1w/d', lt: 'now' } },
+            { name: 'Past month', range: { gte: 'now-1M/d', lt: 'now' } },
+            { name: 'Past year', range: { gte: 'now-1y/d', lt: 'now' } },
+            { name: 'Past decade', range: { gte: 'now-10y/d', lt: 'now' } },
+            { name: 'Custom range...' },
+        ];
+    }),
+
+    actions: {
+        selectRange(range) {
+            this.set('selectedRange', range);
+        }
+    }
 });
--- app/components/search-facet-worktype/component.js
+++ app/components/search-facet-worktype/component.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    selectedType: Ember.computed('query', function() {
+        let query = this.get('query');
+        return query ? query.get(this.get('key')) : undefined;
+    }),
+
+    actions: {
+        change(type) {
+            this.sendAction('onChange', this.get('key'), type);
+        }
+    }
 });
