0964d665f14900eb7acfc3aebb9998f96d486b5e
Lauren Barker <lauren@cos.io>
2016-07-08 18:05:00 -0400

    redirect base to discover page

--- app/router.js
+++ app/router.js
@@ -6,13 +6,14 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('curate', function() {
-      this.route('person', { path: '/person/:person_id' });
-      this.route('work', { path: '/:work_type/:work_id' });
-  });
-  this.route('changes');
-  this.route('discover');
-  this.route('login');
+    this.route('base', {path: '/'});
+    this.route('curate', function() {
+        this.route('person', { path: '/person/:person_id' });
+        this.route('work', { path: '/:work_type/:work_id' });
+    });
+    this.route('changes');
+    this.route('discover');
+    this.route('login');
 });
 
 export default Router;
