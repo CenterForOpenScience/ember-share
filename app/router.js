6ec23a5eeaae2f80a26eecdc7dca40e977b99deb
Lauren Barker <lauren@cos.io>
2016-07-08 18:07:46 -0400

    use index not base

--- app/router.js
+++ app/router.js
@@ -6,7 +6,6 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-    this.route('base', {path: '/'});
     this.route('curate', function() {
         this.route('person', { path: '/person/:person_id' });
         this.route('work', { path: '/:work_type/:work_id' });
