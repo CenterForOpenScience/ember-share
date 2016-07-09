1a5d6fbfb4bfe3ad3f3fbce60a94f740395c86c2
Henrique Moco <hmoco94@vt.edu>
2016-07-09 18:16:11 -0400

    lots of curate and merge work

--- app/components/merge-widget/component.js
+++ app/components/merge-widget/component.js
@@ -4,6 +4,37 @@ import ENV from '../../config/environment';
 export default Ember.Component.extend({
     store: Ember.inject.service(),
     recommendations: [],
+    openModal: false,
+    objAttributes: Ember.computed('obj', function() {
+        var obj = this.get('obj');
+        var ret = [];
+        obj.eachAttribute(
+            attr => {
+                if (obj.get(attr) || this.get('compareTo').get(attr)) {
+                    ret.push(attr);
+                }
+            }
+        );
+        return ret;
+    }),
+    objRelationships: Ember.computed('obj', function() {
+        var obj = this.get('obj');
+        var ret = [];
+        obj.eachRelationship(
+            attr => {
+                if (attr === 'links' || attr === 'changes') {
+                    return;
+                }
+                if ((attr === 'subject') && (obj.get(attr).get('name') || this.get('compareTo').get(attr).get('name'))) {
+                    ret.push(attr);
+
+                } else if (obj.get(attr).get('length') || this.get('compareTo').get(attr).get('length')) {
+                    ret.push(attr);
+                }
+            }
+        );
+        return ret;
+    }),
     init() {
         var _this = this;
         var _type = this.get('_type');
@@ -38,7 +69,14 @@ export default Ember.Component.extend({
     },
     actions: {
         compare(obj) {
-            //pop up comparison modal
+            this.get('store').findRecord(obj['@type'], obj._id).then(model => {
+                this.set('compareTo', model);
+            })
+            this.set('openModal', true);
+        },
+        merge() {
+            //chris pls
+            this.set('openModal', false);
         }
     }
 });
