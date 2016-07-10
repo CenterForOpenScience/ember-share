1bedf743ebb4b15226a8e7180a1c8e18d99feee9
Chris Seto <chriskseto@gmail.com>
2016-07-10 16:11:41 -0400

    Fix import path

--- app/authenticators/osf-token.js
+++ app/authenticators/osf-token.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import ENV from '../../config/environment';
+import ENV from '../config/environment';
 import BaseAuthenticator from 'ember-simple-auth/authenticators/base';
 
 export default BaseAuthenticator.extend({
