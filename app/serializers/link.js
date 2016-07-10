d22e3f2a244af0dab65cc2e8d7a6a31e9d05ec28
Henrique Moco <hmoco94@vt.edu>
2016-07-10 00:41:28 -0400

    add links and clean up contributors to curate page

---
+++ app/serializers/link.js
@@ -0,0 +1,8 @@
+import ApplicationSerializer from './application';
+
+export default ApplicationSerializer.extend({
+    normalize(typeClass, hash) {
+        hash.type = 'link'
+        return this._super(typeClass, hash);
+    },
+});
