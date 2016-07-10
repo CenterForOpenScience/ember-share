007d856214af11c403415a1378a601d4af60b8d6
Abram Booth <abram@cos.io>
2016-07-09 22:37:11 -0400

    Date range picker!

--- app/components/search-facet-daterange/component.js
+++ app/components/search-facet-daterange/component.js
@@ -4,44 +4,62 @@ import moment from 'moment';
 export default Ember.Component.extend({
     init() {
         this._super(...arguments);
-        this.set('displayRange', 'All time');
+        this.set('pickerValue', 'All time');
     },
 
     didInsertElement() {
         this._super(...arguments);
 
         let dateRanges = {
-           'All time': [null, null],
-           'Past week': [moment().subtract(1, 'week'), moment()],
-           'Past month': [moment().subtract(1, 'month'), moment()],
-           'Past year': [moment().subtract(1, 'year'), moment()],
-           'Past decade': [moment().subtract(10, 'year'), moment()]
+            'Past week': [moment().subtract(1, 'week'), moment()],
+            'Past month': [moment().subtract(1, 'month'), moment()],
+            'Past year': [moment().subtract(1, 'year'), moment()],
+            'Past decade': [moment().subtract(10, 'year'), moment()]
         };
 
-        this.$('.date-range').daterangepicker({
-            startDate: null,
-            endDate: null,
-            autoApply: true,
-            ranges: dateRanges
-        }, (start, end, label) => {
+        let picker = this.$('.date-range');
+        let initialRange = dateRanges['Past week'];
+        picker.daterangepicker({
+            ranges: dateRanges,
+            autoUpdateInput: false,
+            locale: { cancelLabel: 'Clear' }
+        });
+
+        picker.on('apply.daterangepicker', (ev, picker) => {
             Ember.run(() => {
-                this.set('displayRange', label);
+                let start = picker.startDate;
+                let end = picker.endDate;
+                if (picker.chosenLabel === 'Custom Range') {
+                    let format = 'Y-MM-DD';
+                    this.set('pickerValue', `${start.format(format)} - ${end.format(format)}`);
+                } else {
+                    this.set('pickerValue', picker.chosenLabel);
+                }
                 this.updateQuery(start, end);
+            });
+        });
+
+        picker.on('cancel.daterangepicker', () => {
+            Ember.run(() => {
+                this.send('clear');
             });
         });
     },
 
     updateQuery(start, end) {
         let key = this.get('key');
-        if (!start.isValid() || !end.isValid()) {
-            this.sendAction('onChange', key, null);
-            return;
-        }
         let queryFilter = { range: {} };
         queryFilter.range[key] = {
             gte: start.format(),
             lte: end.format()
         };
         this.sendAction('onChange', key, queryFilter);
+    },
+
+    actions: {
+        clear() {
+            this.set('pickerValue', 'All time');
+            this.sendAction('onChange', this.get('key'), null);
+        }
     }
 });
