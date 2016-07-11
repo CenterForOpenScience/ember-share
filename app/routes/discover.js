4a1cd968bde879eb9315fd5f577eb427f1585185
Chris Seto <chriskseto@gmail.com>
2016-07-10 23:17:46 -0400

    remove pointless queries

--- app/routes/discover.js
+++ app/routes/discover.js
@@ -1,14 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    session: Ember.inject.service(),
-    model() {
-        return Ember.RSVP.hash({
-            funders: this.store.findAll('funder'),
-            institutions: this.store.findAll('institution'),
-            tags: this.store.findAll('tag'),
-            venues: this.store.findAll('venue'),
-            awards: this.store.findAll('award'),
-        });
-    },
+    session: Ember.inject.service()
 });
