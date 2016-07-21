60687cf708d658ca68c7e3ac56052ec9914e4f97
Lauren Barker <lauren@cos.io>
2016-07-21 16:59:57 -0400

    Add more styling and number of events

--- app/controllers/providers.js
+++ app/controllers/providers.js
@@ -5,12 +5,26 @@ import buildElasticCall from '../utils/build-elastic-call';
 export default Ember.Controller.extend({
     numberOfProviders: 0,
     providers: [],
+    numberOfEvents: 0,
+    eventsLastUpdated: Date().toString(),
 
     init() {
         //TODO Sort initial results on date_modified
         this._super(...arguments);
         this.loadPage();
+        this.loadEventCount();
         // this.set('debouncedLoadPage', _.debounce(this.loadPage.bind(this), 250));
+    },
+    loadEventCount(){
+        var url = ENV.apiUrl + '/api/search/abstractcreativework/_count';
+        return Ember.$.ajax({
+            'url': url,
+            'crossDomain': true,
+            'type': 'GET',
+            'contentType': 'application/json',
+        }).then((json) => {
+            this.set('numberOfEvents', json.count);
+        });
     },
     loadPage(url=null) {
         url = url || ENV.apiUrl + '/api/providers/';
