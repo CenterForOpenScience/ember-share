d968cfc32ef92db02cb5f26c67e9173b661cf46e
karleywalker <kbw2as@virginia.edu>
2016-08-03 12:49:36 -0400

    links for homepage buttons

--- app/components/homepage-button-box/component.js
+++ app/components/homepage-button-box/component.js
@@ -10,12 +10,14 @@ export default Ember.Component.extend({
     classNames: ['btn', 'btn-default', 'btn-sm'],
     classNameBindings: ['selected:active'],
 
-    selected: Ember.computed('selectedType', function() {
+    selected: Ember.computed('filter', function() {
         return this.get('selectedType') === this.get('type');
     }),
 
     click() {
         let type = this.get('selected') ? null : this.get('type');
+        console.log(type);
+        this.transitionTo('share.discover');
         if (!type) {
             this.$().blur();
         }
