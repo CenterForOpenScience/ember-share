b162d2c846e21d2419bb4c4e6ccc572d752cb3bc
Lauren Barker <lauren@cos.io>
2016-08-15 10:58:45 -0400

    Prevent full page reload for homepage blocks

--- app/components/homepage-button-box/component.js
+++ app/components/homepage-button-box/component.js
@@ -9,17 +9,4 @@ export default Ember.Component.extend({
     didRender(){
         Ember.$(".infoBox").parent().css({ "outline": "0"});
     },
-
-    selected: Ember.computed('filter', function() {
-        return this.get('selectedType') === this.get('type');
-    }),
-
-    click() {
-        let type = this.get('selected') ? null : this.get('type');
-        this.transitionTo('share.discover');
-        if (!type) {
-            this.$().blur();
-        }
-        this.sendAction('onClick', type);
-    }
 });
