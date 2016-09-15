72f14b80fa6eedbd5a3f2af68d4f3f9db00734ec
karleywalker <kbw2as@virginia.edu>
2016-09-15 13:39:38 -0400

    registration form

--- app/router.js
+++ app/router.js
@@ -6,16 +6,17 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-    this.route('curate', function() {
-        this.route('person', { path: '/person/:person_id' });
-        this.route('work', { path: '/:work_type/:work_id' });
-    });
-    this.route('changes');
-    this.route('discover');
-    this.route('profile');
-    this.route('support');
-    this.route('settings');
-    this.route('sources');
+  this.route('curate', function() {
+      this.route('person', { path: '/person/:person_id' });
+      this.route('work', { path: '/:work_type/:work_id' });
+  });
+  this.route('changes');
+  this.route('discover');
+  this.route('profile');
+  this.route('support');
+  this.route('settings');
+  this.route('sources');
+  this.route('registration');
 });
 
 export default Router;
