bfecf2839df1933f828a64aa7cd694357f18954a
Lauren Barker <lauren@cos.io>
2016-09-21 15:39:27 -0400

    Update registration --> model and include CSRF token in request

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,15 +3,18 @@ import DS from 'ember-data';
 import ENV from '../config/environment';
 
 export default DS.RESTAdapter.extend(DS.BuildURLMixin, {
+    session: Ember.inject.service(),
     namespace: 'api/v2',
     host: ENV.apiBaseUrl,
     headers: {
         Accept: 'application/json'
     },
     ajax(url, method, hash) {
+        url = url + '/';
         hash = hash || {};
         hash.crossDomain = true;
         hash.xhrFields = { withCredentials: true };
+        hash.headers = { 'X-CSRFTOKEN': this.get('session.data.authenticated.csrfToken') };
         return this._super(url, method, hash);
     },
     pathForType(type) {
