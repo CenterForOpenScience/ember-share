cb8ab6498f511346ffdfb037175be3a8b4d7f0c7
Lauren Barker <lauren@cos.io>
2016-09-23 11:18:58 -0400

    Add session and loginEnabled to form

--- app/controllers/registration/form.js
+++ app/controllers/registration/form.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
+import ENV from '../../config/environment';
 
 export default Ember.Controller.extend({
 
     metrics: Ember.inject.service(),
     store: Ember.inject.service(),
+    session: Ember.inject.service(),
+
+    // TODO: remove when login is enabled on production
+    loginEnabled: ENV.loginEnabled,
 
     actions: {
         submit() {
