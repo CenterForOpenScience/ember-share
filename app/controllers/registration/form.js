b82209301d0047fb2f07357d63f3f99c47622ca9
Lauren Barker <lauren@cos.io>
2016-09-23 16:33:06 -0400

    Show warning page if user has already submitted a form

--- app/controllers/registration/form.js
+++ app/controllers/registration/form.js
@@ -10,9 +10,39 @@ export default Ember.Controller.extend({
     // TODO: remove when login is enabled on production
     loginEnabled: ENV.loginEnabled,
 
+    init() {
+        this._super(...arguments);
+        // if (this.get('session.data.') =>
+        this.getRegistrations();
+        this.set('submitAgain', false);
+    },
+
+    numberOfRegistrations: 0,
+
+    registrationsSubmitted: Ember.computed('session.data.authenticated.user', function() {
+        this.getRegistrations();
+    }),
+
+    submitAgain: false,
+
+    getRegistrations() {
+        var url = ENV.apiUrl + '/registrations/';
+        return Ember.$.ajax({
+            url: url,
+            crossDomain: true,
+            type: 'GET',
+            contentType: 'application/json',
+        }).then((json) => {
+            this.set('numberOfRegistrations', json.count);
+        });
+    },
+
     actions: {
         submit() {
             this.send('saveRegistrationModel', this.get('model'));
+        },
+        submitAgain() {
+            this.set('submitAgain', true);
         }
     }
 
