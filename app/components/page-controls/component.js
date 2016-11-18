0e8a4f8afbd52f54f58f3ad8d6c7e2bfd992b0a5
Abram Booth <abram@cos.io>
2016-11-18 12:44:44 -0500

    Make page-controls care less how pages are implemented

--- app/components/page-controls/component.js
+++ app/components/page-controls/component.js
@@ -1,14 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    page: Ember.computed('offset', 'pageSize', function() {
-        return 1 + Math.floor(this.get('offset') / this.get('pageSize'));
-    }),
-
-    totalPages: Ember.computed('pageSize', 'total', function() {
-        return Math.ceil(this.get('total') / this.get('pageSize'));
-    }),
-
     atFirstPage: Ember.computed.equal('page', 1),
 
     atLastPage: Ember.computed('page', 'totalPages', function() {
@@ -16,14 +8,12 @@ export default Ember.Component.extend({
     }),
 
     actions: {
-        previousPage: function() {
-            const previousOffset = (this.get('page') - 2) * this.get('pageSize');
-            this.get('getPage')(previousOffset);
+        prevPage: function() {
+            this.get('prevPage')();
         },
 
         nextPage: function() {
-            const nextOffset = this.get('page') * this.get('pageSize');
-            this.get('getPage')(nextOffset);
+            this.get('nextPage')();
         }
     }
 });
--- app/components/section-related-works/component.js
+++ app/components/section-related-works/component.js
@@ -8,15 +8,34 @@ export default Ember.Component.extend({
     offset: 0,
     pageSize: 10,
 
-    showPageControls: Ember.computed('model.totalRelatedWorks', 'pageSize', function() {
-        return this.get('model.totalRelatedWorks') > this.get('pageSize');
+    page: Ember.computed('offset', 'pageSize', function() {
+        return 1 + Math.floor(this.get('offset') / this.get('pageSize'));
     }),
 
+    totalPages: Ember.computed('pageSize', 'model.totalRelatedWorks', function() {
+        return Math.ceil(this.get('model.totalRelatedWorks') / this.get('pageSize'));
+    }),
+
+    showPageControls: Ember.computed.gt('totalPages', 1),
+
     actions: {
+        nextPage: function() {
+            const nextOffset = this.get('page') * this.get('pageSize');
+            this.send('getPage', nextOffset);
+        },
+
+        prevPage: function() {
+            const prevOffset = (this.get('page') - 2) * this.get('pageSize');
+            this.send('getPage', prevOffset);
+        },
+
         getPage: function(newOffset) {
+            if (this.get('loadingPage')) {return;}
+            this.set('loadingPage', true);
+
             const model = this.get('model');
             const adapter = this.get('store').adapterFor('application');
-            // TODO consolidate graphql queries in utils or a service or something
+            // TODO consolidate graphql queries in a util or service or something
             adapter.ajax('/api/v2/graph/', 'POST', {
                 data: {
                     variables: '',
@@ -27,12 +46,13 @@ export default Ember.Component.extend({
                     }`
                 }
             }).then(data => {
-                if (data.errors) throw Error(data.errors[0].message);
+                if (data.errors) {throw Error(data.errors[0].message);}
                 this.setProperties({
+                    'loadingPage': false,
                     'model.relatedWorks': data.data.shareObject.relatedWorks,
                     'offset': newOffset
                 });
             });
-        }
+        },
     }
 });
