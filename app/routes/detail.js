fd9d19a66a89fd63c511162910f4681180a12e62
Abram Booth <abram@cos.io>
2016-11-18 15:01:02 -0500

    Plac8 jshint

--- app/routes/detail.js
+++ app/routes/detail.js
@@ -20,35 +20,47 @@ export default Ember.Route.extend({
               }`
             }
         }).then(data => {
-            if (data.errors) throw Error(data.errors[0].message);
+            if (data.errors) {
+                throw Error(data.errors[0].message);
+            }
             return data.data.shareObject;
         });
     },
     actions: {
-        error(error, transition) {
+        error(error) {
             console.error(error);
             return this.intermediateTransitionTo('notfound');
         }
     },
     setup(model, transition) {
-        if (!model) return this._super(model, transition);  // If the model could not be loaded. Do nothing.
+        if (!model) {
+            // If the model could not be loaded. Do nothing.
+            return this._super(model, transition);
+        }
 
         // Find the most specific template available for the found type
         let view = null;
-        for (let i = 0; i < model.types.length; i++)
-          if ((view = CONTROLLER_MAP[model.types[i]])) break;
+        for (let i = 0; i < model.types.length; i++) {
+            if ((view = CONTROLLER_MAP[model.types[i]])) {
+                break;
+            }
+        }
 
         this.set('templateName', view);
         this.set('controllerName', view);
         return this._super(model, transition);
     },
     afterModel(model, transition) {
-        if (!model) return;  // If the model could not be loaded. Do nothing.
+        if (!model) {
+            // If the model could not be loaded. Do nothing.
+            return;
+        }
 
         // If the type slug /:SLUG/:SHARE-ID is not the type of the object
         // Correct the url
         let slug = model.type.classify().toLowerCase();
-        if (slug !== transition.params.detail.type)
+        if (slug !== transition.params.detail.type) {
             return this.transitionTo('detail', slug, model.id);
+        }
     }
 });
