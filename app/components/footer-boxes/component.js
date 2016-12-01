c259e5f664cb30fd6e4c8014ae4b39b840e9bf2a
Lauren Barker <lauren@cos.io>
2016-12-01 15:11:49 -0500

    Update index page responsiveness (#143)

--- app/components/footer-boxes/component.js
+++ app/components/footer-boxes/component.js
@@ -2,13 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-    tagName: 'button',
     classNames: ['ember-view'],
     classNameBindings: ['selected:active'],
-
-    didRender() {
-        Ember.$('.providerBox').parent().css({ background: 'rgba(52, 73, 94, 0)', border: 'none', outline: '0' });
-    },
 
     selected: Ember.computed('selectedType', function() {
         return this.get('selectedType') === this.get('type');
