e73acc22e2593446c7b09e509ea057e303c5f103
Chris Seto <chriskseto@gmail.com>
2016-12-02 22:14:29 -0500

    0 pad converted ids

--- app/routes/curate.js
+++ app/routes/curate.js
@@ -8,7 +8,7 @@ const MOD = 10000000000;
 export default Ember.Route.extend({
     getEncodedPk(pk, type) {
         const modelID = ENV.modelIDs[type] || ENV.modelIDs.creativework;
-        const encoded = new BigInteger(pk).multiply(NUM).mod(MOD).toString(16).toUpperCase();
+        const encoded = ('000000000' + new BigInteger(pk).multiply(NUM).mod(MOD).toString(16).toUpperCase()).substr(-9);
         return `${modelID.toString(16)}${encoded.slice(0, 3)}-${encoded.slice(3, 6)}-${encoded.slice(6)}`;
     },
     beforeModel(transition) {
