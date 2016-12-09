7ce4aaa8536047c2f9a668d3b50520777542a2d9
Abram Booth <abram@cos.io>
2016-12-09 14:53:20 -0500

    Display site banners

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,4 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     session: Ember.inject.service(),
+
+    init() {
+        this._super(...arguments);
+        this.set('banners', this.get('store').findAll('site-banner'));
+    }
 });
---
+++ app/models/site-banner.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    title: DS.attr('string'),
+    description: DS.attr('string'),
+    color: DS.attr('string'),
+    icon: DS.attr('string')
+});
