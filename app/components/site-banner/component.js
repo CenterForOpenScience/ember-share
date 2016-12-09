b7baf68658b7e4e448b1c5fe8c934d4b77c2a599
Abram Booth <abram@cos.io>
2016-12-09 16:29:40 -0500

    Closing banners!

--- app/components/site-banner/component.js
+++ app/components/site-banner/component.js
@@ -1,8 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    store: Ember.inject.service(),
+    init() {
+        this._super(...arguments);
+        this.set('closed', []);
+    },
+
+    openBanners: Ember.computed.setDiff('banners', 'closed'),
 
     actions: {
+        close(banner) {
+            this.get('closed').pushObject(banner);
+        }
     }
 });
