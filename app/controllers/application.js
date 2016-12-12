44eae9981f793b686b4210ee65d06e1d73957074
Abram Booth <abram@cos.io>
2016-12-12 15:06:27 -0500

    Prevent duplicate /api/v2/site_banners request

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,8 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     session: Ember.inject.service(),
 
-    init() {
-        this._super(...arguments);
-        this.set('banners', this.get('store').findAll('site-banner'));
-    }
+    banners: Ember.computed(function() {
+        return this.get('store').findAll('site-banner');
+    }),
 });
