00c0da30eec04ddd44d4ce3d05e7f2ccb5d65d1c
Abram Booth <abram@cos.io>
2016-12-19 15:32:52 -0500

    Finish query syntax primer

--- app/components/query-syntax/component.js
+++ app/components/query-syntax/component.js
@@ -2,14 +2,29 @@ import Ember from 'ember';
 import ENV from '../../config/environment';
 
 export default Ember.Component.extend({
+    showFields: false,
+
     init() {
         this._super(...arguments);
         Ember.$.ajax({
             url: ENV.apiUrl + '/search/_mappings/creativeworks',
             contentType: 'application/json',
         }).then((json) => {
-            // TODO handle nested fields, like `lists`
-            this.set('fields', Object.keys(json.share.mappings.creativeworks.properties).sort());
+            const properties = json.share.mappings.creativeworks.properties;
+            const fields = [];
+            for(const name in properties) {
+                const type = properties[name].type;
+                if (type) {
+                    fields.pushObject({ name, type: properties[name].type});
+                }
+            }
+            this.set('fields', fields.sortBy('name'));
         });
+    },
+
+    actions: {
+        toggleFields() {
+            this.toggleProperty('showFields');
+        }
     }
 });
