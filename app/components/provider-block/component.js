6a0f5d02647cda6a61e140d8ad76896779970093
Abram Booth <abram@cos.io>
2017-01-17 17:29:55 -0500

    Use favicon url from API, don't construct it

--- app/components/provider-block/component.js
+++ app/components/provider-block/component.js
@@ -3,9 +3,6 @@ import ENV from '../../config/environment';
 import { encodeParams } from 'ember-share/utils/elastic-query';
 
 export default Ember.Component.extend({
-    favicon: Ember.computed(function() {
-        return ENV.apiBaseUrl + '/static/' + this.get('source').attributes.providerName + '/img/favicon.ico';
-    }),
     encodedParam: Ember.computed(function() {
         return encodeParams([this.get('source.attributes.longTitle')])[0];
     })
