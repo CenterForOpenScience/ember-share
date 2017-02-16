afb4720c479e60f28170132dd0bf76f9f15b03ab
Lauren Barker <lauren@cos.io>
2017-02-16 15:16:30 -0500

    [SHARE-607][Improvement] Update placeholder for search bar on landing page (#174)

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,7 +5,7 @@ export default Ember.Controller.extend({
 
     metrics: Ember.inject.service(),
 
-    placeholder: 'search events',
+    placeholder: 'search scholarly works',
 
     actions: {
         search() {
