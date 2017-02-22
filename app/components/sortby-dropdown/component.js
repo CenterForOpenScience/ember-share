95b3092d64170ac7b4639ae7aeb4bdbcbd8e21de
Lauren Barker <lauren@cos.io>
2017-02-22 11:21:01 -0500

    Simplify dropdown component
    Remove bold text

--- app/components/sortby-dropdown/component.js
+++ app/components/sortby-dropdown/component.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+    classNames: ['dropdown'],
+
     actions: {
         select(sortBy, display) {
             this.sendAction('selectSortOption', sortBy, display);
