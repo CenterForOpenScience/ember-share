744ce08a24da63b781fe95d541b8c40d78a2a26d
Emily Carden <cardene19@mail.wlu.edu>
2017-06-05 11:59:13 -0400

    rendering id and status

---
+++ tests/unit/helpers/equals-test.js
@@ -0,0 +1,10 @@
+import { equals } from 'ember-share/helpers/equals';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | equals');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = equals([42]);
+  assert.ok(result);
+});
