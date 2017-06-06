80c72848348e806b61fec1161452d81093a0fecf
Oludare Oludare <ooo6hy@virginia.edu>
2017-06-06 09:49:48 -0400

    no message

--- app/controllers/harvest.js
+++ app/controllers/harvest.js
@@ -13,7 +13,7 @@ export default Ember.Controller.extend({
     },
 
     loadPage(url=null) {
-        url = url || ENV.apiUrl + '/harvest/?sort=id';
+        url = url || ENV.apiUrl + '/harvest/';
         this.set('loading', true);
         return Ember.$.ajax({
             url: url,
