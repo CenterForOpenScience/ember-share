a8c0408461a3af1ad36bb73754a0cfba5c66a710
Oludare Oludare <ooo6hy@virginia.edu>
2017-06-08 16:28:22 -0400

    Making API requests only using Routes and models!

---
+++ app/models/harvest-log.js
@@ -0,0 +1,14 @@
+import DS from 'ember-data';
+import Model from 'ember-data/model';
+
+const {
+	attr
+} = DS;
+export default Model.extend({
+	status: attr('string'),
+  context: attr('string'),
+  completions: attr('string'),
+	end_Date: attr('string'),
+  start_Date: attr('string'),
+  harvester_Version: attr('string')
+});
