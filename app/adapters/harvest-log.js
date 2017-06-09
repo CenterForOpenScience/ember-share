8c2f4a6a1d44c3f994001dd9c901b51112ecd550
Emily Carden <cardene19@mail.wlu.edu>
2017-06-09 09:58:24 -0400

    remove hard coding from adapters

--- app/adapters/harvest-log.js
+++ app/adapters/harvest-log.js
@@ -1,10 +1,7 @@
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
-	host: 'http://localhost:8000',
-	namespace: 'api/v2',
-
-	pathForType(){
-		return 'HarvestLog';
-	}
+    pathForType() {
+        return 'Harvest';
+    }
 });
