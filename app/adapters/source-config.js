8408df678602c3113da0274800724e4345a24b7d
Oludare Oludare <ooo6hy@virginia.edu>
2017-06-09 09:11:08 -0400

    commit

---
+++ app/adapters/source-config.js
@@ -0,0 +1,10 @@
+import ApplicationAdapter from './application';
+
+export default ApplicationAdapter.extend({
+	host: 'http://localhost:8000',
+	namespace: 'api/v2',
+
+	pathForType(){
+		return 'SourceConfig';
+	}
+});
---
+++ tests/unit/routes/source-config-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:source-config', 'Unit | Route | source config', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
