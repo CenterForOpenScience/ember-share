e6303487f5eec06a9b2ca0f197e89f01302feef3
Emily Carden <cardene19@mail.wlu.edu>
2017-06-29 12:06:52 -0400

    formatting on record page

--- app/routes/status-board.js
+++ app/routes/status-board.js
@@ -6,10 +6,3 @@ export default Ember.Route.extend({
 		return this.get('store').findAll('source-config');
 	},
 });
-
-
-// store has a query model where you can
-// model would be the list of source configs
-//
-// put a method on the controller or on the model
-// that you can call from the template
