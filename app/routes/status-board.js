89bcf3cbac0a33dc89d72c7c404caaacd0cbdb41
Oludare Olugbemi <ooo6hy@virginia.edu>
2017-07-06 09:25:13 -0400

    moving to components

--- app/routes/status-board.js
+++ app/routes/status-board.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
 	model() {
-		return this.get('store').findAll('source-config');
+		return this.get('store').query('source-config', {page_size: 210});
 	},
 });
