21deaa0d72a2e84b7f722188d0dd8d474d0d5af0
Oludare Olugbemi <ooo6hy@virginia.edu>
2017-07-07 14:10:38 -0400

    Implementation of breadcrumbs navigation

--- app/router.js
+++ app/router.js
@@ -38,7 +38,7 @@ Router.map(function() {
   this.route('notfound', { path: '/*path' });
   this.route('notfound');
   this.route('status-board');
-  this.route('record', { path: '/record/:id' });
+  this.route('harvestloglist', { path: '/harvestloglist/:id' });
   this.route('source');
   this.route('source-config');
 });
