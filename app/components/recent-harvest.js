837445eb12dd58277db15d80529547232c5b20e6
Emily Carden <cardene19@mail.wlu.edu>
2017-07-17 13:14:34 -0400

    recentHarvests as a computed property-- needs formatting

--- app/components/recent-harvest.js
+++ app/components/recent-harvest.js
@@ -1,8 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  recentHarvests: Ember.computed('id', function(){
-    return this.store.query('harvest-log', { source_config_id: this.get('id')} );
+
+  store: Ember.inject.service(),
+
+  recentHarvests: Ember.computed('sourceConfigId', function(){
+    console.log(this.get('sourceConfigId'));
+    return this.get('store').query('harvest-log', { source_config_id: this.get('sourceConfigId')} );
   }),
 
 });
