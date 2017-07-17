a58b753baa057120af829af0ef99bac9a6f7a3bd
Emily Carden <cardene19@mail.wlu.edu>
2017-07-17 14:39:02 -0400

    links between status board and details pages

--- app/components/recent-harvest.js
+++ app/components/recent-harvest.js
@@ -5,7 +5,6 @@ export default Ember.Component.extend({
   store: Ember.inject.service(),
 
   recentHarvests: Ember.computed('sourceConfigId', function(){
-    console.log(this.get('sourceConfigId'));
     return this.get('store').query('harvest-log', { source_config_id: this.get('sourceConfigId')} );
   }),
 
