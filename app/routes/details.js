01bd867f995951264739ed2a0078002b9b9c5aff
Oludare Olugbemi <ooo6hy@virginia.edu>
2017-07-26 14:10:25 -0400

    Breadcrubs fix HarvestLogList

--- app/routes/details.js
+++ app/routes/details.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.get('store').find('harvest-log', params.id );
+    return this.get('store').findRecord('harvest-log', params.id);
   },
+//   setupController(controller, model) {
+//   this._super(controller, model);
+//   this.controllerFor('details').get('id');
+//
+// }
+
 });
