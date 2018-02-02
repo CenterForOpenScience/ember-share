b669ce4cebdd3fdd3abe5bc66f1b26c7cd8fefd9
icereval <icereval@gmail.com>
2018-02-02 12:24:30 -0500

    add firefox tests, and fix chrome

--- testem.js
+++ testem.js
@@ -6,15 +6,21 @@ module.exports = {
     disable_watching: true,
     launch_in_ci: [
         'Chrome',
+        'Firefox',
     ],
     launch_in_dev: [
         'Chrome',
+        'Firefox',
     ],
     browser_args: {
         Chrome: [
             '--headless',
             '--disable-gpu',
             '--remote-debugging-port=9222',
+            '--no-sandbox',
+        ],
+        Firefox: [
+            '-headless',
         ],
     },
 };
