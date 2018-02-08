b6bdd17dd0818895f1cb8634f2b1630109d385e1
Barrett K. Harber <barrett.harber@gmail.com>
2018-02-08 11:39:23 -0500

    Add Raven client for production builds

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -26,6 +26,21 @@ module.exports = function(defaults) {
                 'node_modules/c3',
             ],
         },
+        inlineContent: {
+            raven: {
+                enabled: LEAN_BUILD,
+                content: `
+                    <script src="https://cdn.ravenjs.com/3.22.1/ember/raven.min.js"></script>
+                    <script>
+                        var encodedConfig = document.head.querySelector("meta[name$='/config/environment']").content;
+                        var config = JSON.parse(unescape(encodedConfig));
+                        if (config.sentryDSN) {
+                            Raven.config(config.sentryDSN, config.sentryOptions || {}).install();
+                        }
+                    </script>
+                `.trim().replace(/^\s{16}/gm, ''),
+            },
+        },
         'ember-cli-babel': {
             includePolyfill: true,
         },
